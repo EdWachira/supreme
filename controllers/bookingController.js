7c301f271d2c2227cff9617f16e04d3a1c965c99
EdWachira <edwinwachira007@gmail.com>
2022-11-18 00:23:46 +0300

    Modified package.json

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -20,16 +20,23 @@ exports.getCheckoutSessuion = catchAsync(async (req, res, next) => {
     client_reference_id: req.params.tourId,
     line_items: [
       {
-        name: `${tour.name} Tour`,
-        description: tour.summary,
-        images: [
-          `${req.protocol}://${req.get('host')}/img/tours/${tour.imageCover}`,
-        ],
-        amount: tour.price * 100,
-        currency: 'usd',
+        price_data: {
+          currency: "usd",
+          unit_amount: tour.price * 100,
+          product_data: {
+            name: `${tour.name} Tour`,
+            description: tour.summary,
+            images: [
+              `${req.protocol}://${req.get("host")}/img/tours/${
+                tour.imageCover
+              }`,
+            ],
+          },
+        },
         quantity: 1,
       },
     ],
+    mode: "payment",
   });
 
   res.status(200).json({
