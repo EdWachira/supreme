c8410f670c5b85836b5a63310a5f1ef5d522162d
EdWachira <edwinwachira007@gmail.com>
2022-11-18 10:02:09 +0300

    Modified package.json

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -1,6 +1,7 @@
 /* eslint-disable */
 import axios from "axios";
 import { showAlert } from "./alerts";
+import {loadStripe} from '@stripe/stripe-js';
 const Stripe = require('stripe');
 
 
@@ -8,7 +9,7 @@ export const bookTour = async (tourId) => {
   try {
     const stripe = window.Stripe('pk_test_51M40IKLWFD9go2aioOdXJZYnWOzR9PJHxyxaXdNmXrItzdXkjExpnIHZsjotSudHyPINKdIk89OcL39bciMV074i00ydnnIvzS');
     const session = await axios(
-      `/api/v1/bookings/checkout-session/${tourId}`
+      `https://safe-waters-68990.herokuapp.com/bookings/checkout-session/${tourId}`
     );
 
     await stripe.redirectToCheckout({
